200dd294c7e35ce0f88601696b1164be5ea2d6ae
piyo2018 <ishii039@gmail.com>
2018-08-24 01:05:43 +0900

    [add]メッセージと計算結果を出力する機能を追加

---
+++ 00-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("てんぷら");
+		System.out.println("江戸三味の一つです");
+		System.out.println("日本国内外に広がっています");
+		System.out.println("150+130の計算をします");
+		System.out.println(150+130);
+
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
+
+	}
+
+}
