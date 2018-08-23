ecb9e6fc9ca51c551e60dcd56ec38098712e9ab1
piyo2018 <ishii039@gmail.com>
2018-08-24 01:07:49 +0900

    [add]長方形の面積を計算する機能を追加

---
+++ 01-Kiso/src/Practice1.java
@@ -0,0 +1,13 @@
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 5;
+		int area = 3 * 5;
+		System.out.println("縦幅" + x + " 横幅" + y + "の長方形の面積は、" + area);
+
+
+	}
+
+}
