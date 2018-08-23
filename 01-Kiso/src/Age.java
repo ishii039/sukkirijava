d3baf46b818a3f65a8e6585822d3a4ed62bdd1c3
piyo2018 <ishii039@gmail.com>
2018-08-24 01:07:09 +0900

    [add]年齢を出力する機能を追加

---
+++ 01-Kiso/src/Age.java
@@ -0,0 +1,12 @@
+
+public class Age {
+
+	public static void main(String[] args) {
+		int age;
+		age = 15;
+		System.out.println("私の年齢は" + age);
+		age = 16;
+		System.out.println("来年は" + age);
+	}
+
+}
