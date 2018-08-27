bccccbcdd4580781894296919a41a0c83789e1cd
piyo2018 <ishii039@gmail.com>
2018-08-27 17:50:14 +0900

    [add]来年の年齢を教えてくれる機能を追加

---
+++ 02-FormulaAndOperator/src/StringInt.java
@@ -0,0 +1,11 @@
+
+public class StringInt {
+	public static void main(String[] args) {
+		String age = "15";
+		int n = Integer.parseInt(age);
+		System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+
+
+	}
+
+}
