230d6c212271585bb1760c8c997fa1e412bf5217
piyo2018 <ishii039@gmail.com>
2018-08-27 17:19:15 +0900

    [add]Math.maxを利用して数字の大きさを比較する機能を追加

---
+++ 02-FormulaAndOperator/src/Num.java
@@ -0,0 +1,12 @@
+
+public class Num {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		int m = Math.max(a,b);
+		System.out.println("比較実験：" + a + "とで大きい方は…" + m);
+
+	}
+
+}
