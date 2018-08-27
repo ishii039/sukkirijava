98b523b0b7b79bca828a9da4cf6b2aa465e4b069
piyo2018 <ishii039@gmail.com>
2018-08-27 17:20:34 +0900

    [add]System.outを利用してメッセージを表示させる機能を追加

---
+++ 02-FormulaAndOperator/src/InstructionExecution.java
@@ -0,0 +1,18 @@
+
+public class InstructionExecution {
+	public static void main(String[] args) {
+		String name = "田中";
+		String message;
+		message = name + "さん、こんにちは";
+		System.out.println(message);
+
+		String myName  = "山本";
+		System.out.print("私の名前は");
+		System.out.print(myName);
+		System.out.print("です");
+
+
+
+	}
+
+}
