ae2546caf84c35fab07b6697556c5e2837201a64
piyo2018 <ishii039@gmail.com>
2018-08-27 17:25:18 +0900

    [add]型変換し、年齢を表示させる機能を追加

---
+++ 02-FormulaAndOperator/src/TypeConversion.java
@@ -0,0 +1,26 @@
+
+public class TypeConversion {
+
+	public static void main(String[] args) {
+		float f = 3;
+		double d = f;
+		System.out.println(f);
+		System.out.println(d);
+
+		int age = (int)3.2;
+		System.out.println(age);
+
+		double dd = 8.5 / 2;
+		long l = 5 + 2L;
+		System.out.println(dd);
+		System.out.println(l);
+
+		String msg = "私の年齢は" + 23;
+		System.out.println(msg);
+		
+		
+
+
+	}
+
+}
