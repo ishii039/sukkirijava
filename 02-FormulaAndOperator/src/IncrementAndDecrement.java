8a510c1f56894d6c1031b678b0ca21c1327ec602
piyo2018 <ishii039@gmail.com>
2018-08-27 16:25:29 +0900

    [add]変数を追加し、メッセージを追加

--- 02-FormulaAndOperator/src/IncrementAndDecrement.java
+++ 02-FormulaAndOperator/src/IncrementAndDecrement.java
@@ -2,10 +2,15 @@
 public class IncrementAndDecrement {
 
 	public static void main(String[] args) {
-		int a;
-		a = 100;
-		a++;
-		System.out.println(a);
+		int c;
+		c = 100;
+		c++;
+		System.out.println(c);
+
+		int a = 10;
+		int b = 10;
+		System.out.println(++a + 50);
+		System.out.println(b++ + 50);
 
 	}
 
